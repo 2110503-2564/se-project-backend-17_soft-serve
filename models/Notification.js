22a52b0b0ebe28e26d0b4f9a5139fcad812b04fc
isandwish <natthanicha.jjr@gmail.com>
2025-04-11 21:54:21 +0700

    Add Notification model schema

---
+++ models/Notification.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const NotificationSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      creatorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'createdBy' 
+      },
+      createdBy: {
+        type: String,
+        enum: ['Admin', 'RestaurantManager'],
+        required: true
+      },
+      targetAudience: {
+        type: String,
+        // Restaurant Manager can only sent to customers that are has reserved in his restaurant
+        // Admin can sent to customers or restaurant managers or all users
+        enum: ['Customers', 'RestaurantManagers', 'All'],
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+module.exports = mongoose.model('Notification', NotificationSchema);
